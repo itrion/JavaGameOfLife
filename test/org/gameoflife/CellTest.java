ef9af271988bdf968975ee69dd59fdcf74824852
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-03-07 18:39:25 +0000

    created cell test

---
+++ test/org/gameoflife/CellTest.java
@@ -0,0 +1,15 @@
+package org.gameoflife;
+
+import org.junit.Test;
+
+public class CellTest {
+
+    @Test
+    public void testCellCreation(){}
+
+    @Test
+    public void testCellDie(){}
+
+    @Test
+    public void testCellAlive(){}
+}
