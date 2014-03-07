13692e89bfd3a3fbff3f17db58b508d4e347360a
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-03-07 18:48:57 +0000

    created Cell test

--- test/org/gameoflife/CellTest.java
+++ test/org/gameoflife/CellTest.java
@@ -5,11 +5,24 @@ import org.junit.Test;
 public class CellTest {
 
     @Test
-    public void testCellCreation(){}
+    public void testCellCreation(){
+        Cell cell = new Cell();
+        assertEquals(State.ALIVE, cell.getState());
+    }
 
     @Test
-    public void testCellDie(){}
+    public void testCellDie(){
+        Cell cell = new Cell();
+        cell.kill();
+        assertEquals(State.DEAD, cell.getState());
+    }
 
     @Test
-    public void testCellAlive(){}
+    public void testCellAlive(){
+        Cell cell = new Cell();
+        cell.kill();
+        cell.resurrect();
+        assertEquals(State.ALIVE, cell.getState());
+
+    }
 }
